62c9fef6ebaa75db30bb05ed8cd3960ba47e487d
Benjamin Bastian <benjamin.bastian@gmail.com>
2017-08-08 20:49:15 -0700

    Add densenet model

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torchvision
+
+class DenseNet(nn.Module):
+    def __init__(self):
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 3, kernel_size=1, stride=1),
+            nn.BatchNorm2d(3),
+            nn.ReLU(inplace=True)
+        )
+        self.densenet = torchvision.models.densenet201(
+            pretrained=False,
+            num_classes=7
+        )
+
+    def forward(self, minibatch):
+        return self.densenet(self.conv(minibatch))
