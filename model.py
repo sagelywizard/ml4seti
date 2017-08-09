a9eb9dd04d3f359e01bc760501fb78b0b0c0e492
Benjamin Bastian <benjamin.bastian@gmail.com>
2017-08-08 22:26:28 -0700

    initialize model

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torchvision
 
 class DenseNet(nn.Module):
     def __init__(self):
+        super(DenseNet, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(1, 3, kernel_size=1, stride=1),
             nn.BatchNorm2d(3),
